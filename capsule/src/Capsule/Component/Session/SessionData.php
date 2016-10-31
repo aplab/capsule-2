452ff367ecfd5e2502a97dfbd9454169fc17b0a3
polyanin <polyanin@gmail.com>
2016-11-01 02:14:21 +0500

    session data

---
+++ capsule/src/Capsule/Component/Session/SessionData.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Capsule package.
+ *
+ * (c) Alexander Polyanin 2006 <polyanin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Date: 01.11.2016
+ * Time: 2:08
+ */
+
+namespace Capsule\Component\Session;
+
+
+class SessionData
+{
+
+}
