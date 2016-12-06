badd6af24e5b4eb46219ff4ef988e2ce4cdc1454
polyanin <polyanin@gmail.com>
2016-12-06 23:27:26 +0500

    trying bootstrap 4 alpha 5 unsuccessfull, rejected

--- capsule/templates/App/Cms/View/ObjectEditorView/template.php
+++ capsule/templates/App/Cms/View/ObjectEditorView/template.php
@@ -29,7 +29,44 @@ $prefix = 'capsule-cms-object-editor';
     <div class="<?=$prefix?>-body">
         <?php foreach ($this->instance->groups as $group) : ?>
             <div class="<?=$prefix?>-panel">
-                <?php \Capsule\Tools\Tools::dump($group) ?>
+                <?php #\Capsule\Tools\Tools::dump($group) ?>
+                <div class="<?=$prefix?>-elements-wrapper">
+                    <div class="<?=$prefix?>-elements">
+
+                        <div class="form-horizontal">
+                            <div class="form-group">
+                                <label class="control-label col-sm-3" for="email">Email:</label>
+                                <div class="col-sm-9">
+                                    <input type="email" class="form-control" id="email" placeholder="Enter email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-3" for="pwd">Password:</label>
+                                <div class="col-sm-9">
+                                    <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-9">
+                                    <div class="checkbox">
+                                        <label><input type="checkbox"> Remember me</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-3" for="comment">Password:</label>
+                                <div class="col-sm-9">
+                                    <textarea class="form-control" rows="5" id="comment"></textarea>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+                    </div>
+                </div>
             </div>
         <?php endforeach ?>
     </div>
