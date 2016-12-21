f6a58fc4e1f753b28aa1a2fc1acf0e085ffdda66
polyanin <polyanin@gmail.com>
2016-12-21 23:10:41 +0500

    maximize test

--- capsule/templates/App/Cms/Ui/Section/dialog.php
+++ capsule/templates/App/Cms/Ui/Section/dialog.php
@@ -14,7 +14,7 @@
 <div class="capsule-cms-dialog" id="test-dialog">
     <div class="capsule-cms-dialog-backdrop"></div>
     <div class="capsule-cms-dialog-window">
-        <div class="capsule-cms-dialog-content">
+        <div class="capsule-cms-dialog-content capsule-cms-dialog-maximize">
             <div class="capsule-cms-dialog-header">
                 header
             </div>
